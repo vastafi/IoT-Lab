bc9800784c47268b1ce693759459dbed49ae738c
vastafi <valentina.astafi@isa.utm.md>
2023-10-31 10:14:39 +0200

    deleting unnecessary comments

--- src/dd_dht/dd_dht.cpp
+++ src/dd_dht/dd_dht.cpp
@@ -1,12 +1,3 @@
-// DHT Temperature & Humidity Sensor
-// Unified Sensor Library Example
-// Written by Tony DiCola for Adafruit Industries
-// Released under an MIT license.
-
-// REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
-
 #include <Arduino.h>
 
 #include "dd_dht.h"
@@ -18,16 +9,7 @@
 #include "../lib_cond/lib_cond.h"
 #include "../lib_cond/lib_cond.h"
 
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
-
-// Uncomment the type of sensor in use:
 #define DHTTYPE DHT11 // DHT 11
-// #define DHTTYPE    DHT22     // DHT 22 (AM2302)
-// #define DHTTYPE    DHT21     // DHT 21 (AM2301)
-
-// See guide for details on sensor wiring and usage:
-//   https://learn.adafruit.com/dht/overview
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
@@ -67,24 +49,10 @@ void dd_dht_setup()
   dht.temperature().getSensor(&sensor);
   // Serial.println(F("------------------------------------"));
   // Serial.println(F("Temperature Sensor"));
-  // Serial.print  (F("Sensor Type: ")); //Serial.println(sensor.name);
-  // Serial.print  (F("Driver Ver:  ")); //Serial.println(sensor.version);
-  // Serial.print  (F("Unique ID:   ")); //Serial.println(sensor.sensor_id);
-  // Serial.print  (F("Max Value:   ")); //Serial.print(sensor.max_value); //Serial.println(F("°C"));
-  // Serial.print  (F("Min Value:   ")); //Serial.print(sensor.min_value); //Serial.println(F("°C"));
-  // Serial.print  (F("Resolution:  ")); //Serial.print(sensor.resolution); //Serial.println(F("°C"));
-  // Serial.println(F("------------------------------------"));
-  //  Print humidity sensor details.
+ 
   dht.humidity().getSensor(&sensor);
   // Serial.println(F("Humidity Sensor"));
-  // Serial.print  (F("Sensor Type: ")); //Serial.println(sensor.name);
-  // Serial.print  (F("Driver Ver:  ")); //Serial.println(sensor.version);
-  // Serial.print  (F("Unique ID:   ")); //Serial.println(sensor.sensor_id);
-  // Serial.print  (F("Max Value:   ")); //Serial.print(sensor.max_value); //Serial.println(F("%"));
-  // Serial.print  (F("Min Value:   ")); //Serial.print(sensor.min_value); //Serial.println(F("%"));
-  // Serial.print  (F("Resolution:  ")); //Serial.print(sensor.resolution); //Serial.println(F("%"));
-  // Serial.println(F("------------------------------------"));
-  //  Set delay between sensor readings based on sensor details.
+
   delayMS = sensor.min_delay / 1000;
 }
 
--- src/lib_cond/lib_cond.cpp
+++ src/lib_cond/lib_cond.cpp
@@ -1,5 +1,3 @@
-
-
 #include "lib_cond.h"
 #include <Arduino.h>
 
